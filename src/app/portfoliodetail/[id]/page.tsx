9800828f823b2c4ebaa56f8880685450b77b510a
Nibir97 <moinul824@gmail.com>
2025-06-18 22:57:42 +0600

    changes done

--- src/app/portfoliodetail/[id]/page.tsx
+++ src/app/portfoliodetail/[id]/page.tsx
@@ -1,26 +1,29 @@
-'use client';
 import React from 'react';
 import PorfolioDetailDesign from '../../page/portfolio-detail-design';
 import { DataArray } from '@/app/data';
-import { useParams } from 'next/navigation';
 
-const Portfolio = () => {
-  const params = useParams();
-  const id = typeof params?.id === 'string' ? params.id : Array.isArray(params?.id) ? params.id[0] : '';
+interface PortfolioProps {
+  params: {
+    id: string;
+  };
+}
+
+const Portfolio = ({ params }: PortfolioProps) => {
+  const { id } = params;
 
   if (!id || !DataArray[id]) {
-    return <div>Invalid portfolio ID</div>
+    return <div>Invalid portfolio ID</div>;
   }
 
-  return(
+  return (
     <>
       <PorfolioDetailDesign
-      data={DataArray[id]}
-      id={id}
-      DataArray={DataArray}
+        data={DataArray[id]}
+        id={id}
+        DataArray={DataArray}
       />
     </>
   );
 };
 
-export default Portfolio
+export default Portfolio;
